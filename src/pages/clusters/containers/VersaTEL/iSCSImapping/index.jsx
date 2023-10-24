adf0ba90aa36f60a2e1e5b541a23e482cf710ad3
a3hen <841361233@qq.com>
2023-10-24 11:48:13 +0800

    完成基础框架的搭建

---
+++ src/pages/clusters/containers/VersaTEL/iSCSImapping/index.jsx
@@ -0,0 +1,168 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import { toJS } from 'mobx'
+import { get, omit } from 'lodash'
+
+import { Avatar, Status } from 'components/Base'
+import Banner from 'components/Cards/Banner'
+import Table from 'components/Tables/List'
+import withList, { ListPage } from 'components/HOCs/withList'
+
+import { getDisplayName } from 'utils'
+import { getSuitableValue, getValueByUnit } from 'utils/monitoring'
+import { ICON_TYPES } from 'utils/constants'
+
+import iSCSIMappingStore from 'stores/iSCSImapping'
+
+// import ProjectStore from 'stores/project'
+// import ProjectMonitorStore from 'stores/monitoring/project'
+
+// const MetricTypes = {
+//   cpu: 'namespace_cpu_usage',
+//   memory: 'namespace_memory_usage_wo_cache',
+//   pod: 'namespace_pod_count',
+// }
+
+@withList({
+  store: new iSCSIMappingStore(),
+  name: 'iSCSIMapping',
+  module: 'iSCSImapping',
+})
+export default class iSCSIMapping extends React.Component {
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.props.tableProps.tableActions.onFetch({ silent: true })
+    }, 2000)
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval)
+    }
+  }
+
+  showAction = record => !record.isFedManaged
+
+  get itemActions() {
+    const { trigger, routing, store, tableProps } = this.props
+    return [
+      {
+        key: 'create_snapshot',
+        icon: 'pen',
+        text: t('CREATE_SNAPSHOT'),
+        action: 'edit',
+        show: this.showAction,
+        onClick: item => {
+          // console.log(item)
+          trigger('snapshot.create', {
+            SnapshotTemplatesTemplates: toJS(store.SnapshotTemplates.data),
+            // success: getData,
+            resourcename: item?.name,
+          })
+        },
+      },
+    ]
+  }
+
+  get tabs() {
+    return {
+      value: this.props.module,
+      // value: this.type || 'snapshot',
+      onChange: this.handleTabChange,
+      options: [
+        {
+          value: 'iSCSImapping',
+          label: t('INITIATOR'),
+        },
+        {
+          value: 'iSCSImapping1',
+          label: t('iSCSITARGET'),
+        },
+        {
+          value: 'iSCSImapping2',
+          label: t('MAPPING'),
+        },
+      ],
+    }
+  }
+
+  get tableActions() {
+    const { tableProps } = this.props
+    return {
+      ...tableProps.tableActions,
+      onCreate: this.showCreate,
+      onFetch: this.handleFetch,
+      selectActions: [],
+      createText: '注册',
+    }
+  }
+
+  handleFetch = (params, refresh) => {
+    const { routing } = this.props
+    routing.query({ ...params, type: this.type }, refresh)
+  }
+
+  handleTabChange = type => {
+    const { cluster } = this.props.match.params
+    this.props.routing.push(`/clusters/${cluster}/${type}`)
+  }
+
+  getColumns = () => {
+    const { module } = this.props
+    return [
+      {
+        title: t('Hostname'),
+        dataIndex: 'name',
+        width: '50%',
+        render: name => name,
+      },
+      {
+        title: t('IQN'),
+        dataIndex: 'numbers',
+        width: '50%',
+        render: numbers => numbers,
+      },
+    ]
+  }
+
+  showCreate = () =>
+    this.props.trigger('snapshot.create', {
+      ...this.props.match.params,
+      success: () => this.props.getData,
+    })
+
+  render() {
+    const { bannerProps, tableProps } = this.props
+    return (
+      <ListPage {...this.props} module="namespaces">
+        <Banner {...bannerProps} tabs={this.tabs} />
+        <Table
+          {...tableProps}
+          itemActions={this.itemActions}
+          tableActions={this.tableActions}
+          columns={this.getColumns()}
+          rowSelection={undefined}
+          searchType="name"
+          hideSearch={true}
+        />
+      </ListPage>
+    )
+  }
+}
---
+++ src/pages/clusters/containers/VersaTEL/iSCSImapping2/index.jsx
@@ -0,0 +1,195 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import { toJS } from 'mobx'
+import { get, omit } from 'lodash'
+
+import { Avatar, Status } from 'components/Base'
+import Banner from 'components/Cards/Banner'
+import Table from 'components/Tables/List'
+import withList, { ListPage } from 'components/HOCs/withList'
+
+import { getDisplayName } from 'utils'
+import { getSuitableValue, getValueByUnit } from 'utils/monitoring'
+import { ICON_TYPES } from 'utils/constants'
+
+import SnapshotStore from 'stores/snapshot'
+
+// import ProjectStore from 'stores/project'
+// import ProjectMonitorStore from 'stores/monitoring/project'
+
+// const MetricTypes = {
+//   cpu: 'namespace_cpu_usage',
+//   memory: 'namespace_memory_usage_wo_cache',
+//   pod: 'namespace_pod_count',
+// }
+
+@withList({
+  store: new SnapshotStore(),
+  name: 'Snapshot',
+  module: 'snapshot',
+})
+export default class Snapshot extends React.Component {
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.props.tableProps.tableActions.onFetch({ silent: true })
+    }, 2000)
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval)
+    }
+  }
+
+  showAction = record => !record.isFedManaged
+
+  get itemActions() {
+    const { trigger, routing, store, tableProps } = this.props
+    return [
+      {
+        key: 'create_snapshot',
+        icon: 'pen',
+        text: t('CREATE_SNAPSHOT'),
+        action: 'edit',
+        show: this.showAction,
+        onClick: item => {
+          // console.log(item)
+          trigger('snapshot.create', {
+            SnapshotTemplatesTemplates: toJS(store.SnapshotTemplates.data),
+            // success: getData,
+            resourcename: item?.name,
+          })
+        },
+      },
+      // {
+      //   key: 'create_scheduled_snapshot',
+      //   icon: 'pen',
+      //   text: t('CREATE_SHEDULED_SNAPSHOT'),
+      //   action: 'manage',
+      //   show: record => !record.workspace && this.showAction(record),
+      //   onClick: item => {
+      //     // console.log(item)
+      //     trigger('snapshot_scheduled.create', {
+      //       SnapshotTemplatesTemplates: toJS(store.SnapshotTemplates.data),
+      //       // success: getData,
+      //       resourcename: item?.name,
+      //     })
+      //   },
+      // },
+      // {
+      //   key: 'delete',
+      //   icon: 'trash',
+      //   text: t('DELETE'),
+      //   action: 'delete',
+      //   show: record => !record.workspace && this.showAction(record),
+      //   onClick: item => {
+      //     // console.log(item)
+      //     trigger('snapshot_resource.delete', {
+      //       SnapshotTemplatesTemplates: toJS(store.SnapshotTemplates.data),
+      //       // success: getData,
+      //       resourcename: item?.name,
+      //     })
+      //   },
+      // },
+    ]
+  }
+
+  get tabs() {
+    console.log('snapshot_props', this.props)
+    return {
+      value: this.props.module,
+      // value: this.type || 'snapshot',
+      onChange: this.handleTabChange,
+      options: [
+        {
+          value: 'snapshot',
+          label: t('SNAPSHOT_RESOURCE'),
+        },
+        {
+          value: 'ssnapshot',
+          label: t('SNAPSHOT_SNAPSHOT'),
+        },
+      ],
+    }
+  }
+
+  get tableActions() {
+    const { tableProps } = this.props
+    return {
+      ...tableProps.tableActions,
+      onFetch: this.handleFetch,
+      selectActions: [],
+    }
+  }
+
+  handleFetch = (params, refresh) => {
+    const { routing } = this.props
+    routing.query({ ...params, type: this.type }, refresh)
+  }
+
+  handleTabChange = type => {
+    const { cluster } = this.props.match.params
+    this.props.routing.push(`/clusters/${cluster}/${type}`)
+  }
+
+  getColumns = () => {
+    const { module } = this.props
+    return [
+      {
+        title: t('Resource'),
+        dataIndex: 'name',
+        width: '50%',
+        render: name => name,
+      },
+      {
+        title: t('Snapshot_Numbers'),
+        dataIndex: 'numbers',
+        width: '50%',
+        render: numbers => numbers,
+      },
+    ]
+  }
+
+  showCreate = () =>
+    this.props.trigger('snapshot.create', {
+      ...this.props.match.params,
+      success: () => this.props.getData,
+    })
+
+  render() {
+    const { bannerProps, tableProps } = this.props
+    return (
+      <ListPage {...this.props} module="namespaces">
+        <Banner {...bannerProps} tabs={this.tabs} />
+        <Table
+          {...tableProps}
+          itemActions={this.itemActions}
+          tableActions={this.tableActions}
+          columns={this.getColumns()}
+          rowSelection={undefined}
+          // onCreate={this.type === 'snapshot' ? null : this.showCreate}
+          // isLoading={tableProps.isLoading || isLoadingMonitor}
+          searchType="name"
+          hideSearch={true}
+        />
+      </ListPage>
+    )
+  }
+}
