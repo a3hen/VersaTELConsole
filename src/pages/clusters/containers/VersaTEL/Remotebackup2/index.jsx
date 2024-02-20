1782eb1f083d33604bcbbd99222ef08080e3e95a
a3hen <841361233@qq.com>
2024-02-20 14:26:16 +0800

    补连不上controller判定

--- src/pages/clusters/containers/VersaTEL/Remotebackup2/index.jsx
+++ src/pages/clusters/containers/VersaTEL/Remotebackup2/index.jsx
@@ -175,18 +175,39 @@ export default class Remotebackup1 extends React.Component {
 
   render() {
     const { bannerProps, tableProps } = this.props
+    const error = tableProps.data[0]?.error
+    const ipPortRegex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+)/
+    const match = error?.match(ipPortRegex)
+    const ipPort = match ? match[0] : ''
+
+    const LoadingComponent = () => (
+      <div style={{ textAlign: 'center' }}>
+        <strong style={{ fontSize: '20px' }}>Loading...</strong>
+        <p>无法连接至controller ip：{ipPort}</p>
+      </div>
+    )
+
+    // 检查store中的数据是否包含error属性
+    const isLoading = tableProps.data.some(item => item.error)
+
     return (
       <ListPage {...this.props} module="namespaces">
         <Banner {...bannerProps} tabs={this.tabs} />
-        <Table
-          {...tableProps}
-          itemActions={this.itemActions}
-          tableActions={this.tableActions}
-          columns={this.getColumns()}
-          rowSelection={undefined}
-          searchType="name"
-          hideSearch={true}
-        />
+        {isLoading ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <LoadingComponent />
+          </div>
+        ) : (
+          <Table
+            {...tableProps}
+            itemActions={this.itemActions}
+            tableActions={this.tableActions}
+            columns={this.getColumns()}
+            rowSelection={undefined}
+            searchType="name"
+            hideSearch={true}
+          />
+        )}
       </ListPage>
     )
   }
